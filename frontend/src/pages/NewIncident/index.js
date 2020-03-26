1ef91b8f16ad26f215bb1121402d3184929a7534
“Marina <>
2020-03-26 12:39:36 +0000

    FEAT: front-end with static data

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
+
+export default function NewIncident() {
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+          </p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02042" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em euros" />
+
+          {/* <button className="button" type="submit">
+            Cancelar
+          </button> */}
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
